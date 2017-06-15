2b4b6f316b0d92d2e2acd36084302fe5979fb836
Peter Schmidt-Kallesøe <pkallesoe@gmail.com>
2017-06-15 23:16:20 +0200

    Add evil to emacs

--- emacs.d/settings.el
+++ emacs.d/settings.el
@@ -33,3 +33,55 @@
 
 (use-package magit
   :ensure t)
+
+(use-package evil
+   :ensure t
+   :bind ("C-<f1>" . evil-mode))
+
+(use-package evil-leader
+      :commands (evil-leader-mode)
+      :ensure evil-leader
+      :demand evil-leader
+      :init
+      (global-evil-leader-mode)
+      :config
+      (progn
+        (evil-leader/set-leader ",")
+        ;; bindings from earlier
+        )
+;; keyboard shortcuts
+        (evil-leader/set-key
+          "a" 'ag-project
+          "A" 'ag
+          "b" 'ido-switch-buffer
+          "c" 'mc/mark-next-like-this
+          "C" 'mc/mark-all-like-this
+          "e" 'er/expand-region
+          "E" 'mc/edit-lines
+          "f" 'ido-find-file
+          "g" 'magit-status
+          "i" 'idomenu
+          "j" 'ace-jump-mode
+          "k" 'kill-buffer
+          "K" 'kill-this-buffer
+          "o" 'occur
+          "p" 'magit-find-file-completing-read
+          "r" 'recentf-ido-find-file
+          "s" 'ag-project
+          "t" 'ansi-term
+          "T" 'eshell
+          "w" 'save-buffer
+          "x" 'smex
+      )
+)
+
+(use-package evil-escape
+   :config (evil-escape-mode t)
+   :config(setq-default evil-escape-key-sequence "jk"))
+(evil-mode 1)
+
+;; Easy navigation between windows.
+     (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
